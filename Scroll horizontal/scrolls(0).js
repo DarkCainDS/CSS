60bbfe8d9365ff53160a16533a0de1ae506f02a9
Diego Romero <minirana100@gmail.com>
2022-09-25 02:23:15 -0300

    Wavy-Letter, Scroll-Horizontal

---
+++ Scroll horizontal/scrolls(0).js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+export const scrolls = () => {
+    return (
+        <div className='home'>
+          <div className='media-scroller snaps-inline'>
+            <div className='media-element'>
+              <img src="/images/Final fantasy.png" alt="Final Fantasy" />
+              <p>Final Fantasy 1</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/Final fantasy 2.jpg" alt="Final Fantasy" />
+              <p>Final Fantasy 2</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/Final fantasy 3.jpg" alt="Final Fantasy" />
+              <p>Final Fantasy 3</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/Final fantasy 4.jpg" alt="Final Fantasy" />
+              <p>Final Fantasy 4</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/Final fantasy 5.jpg" alt="Final Fantasy" />
+              <p>Final Fantasy 5</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/Final fantasy 6.jpeg" alt="Final Fantasy" />
+              <p>Final Fantasy 6</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/Final-Fantasy-7-Concept-Art-Cover.jpg" alt="Final Fantasy" />
+              <p>Final Fantasy 7</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/gilang-febrian-oppasa-beach.jpg" alt="Chrono Cross" />
+              <p>Chrono Cross</p>
+            </div>
+            <div className='media-element'>
+              <img src="/images/wallpaperflare.com_wallpaper.jpg" alt="Metroid" />
+              <p>Metroid</p>
+            </div>
+          </div>
+    
+          <h2>Groups elements</h2>
+          <div className='media-scroller--with-groups snaps-inline'>
+            <div className='media-group'>
+              <div className='media-element'>
+                <img src="/images/Final fantasy.png" alt="Final Fantasy" />
+                <p>Final Fantasy 1</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/Final fantasy 2.jpg" alt="Final Fantasy" />
+                <p>Final Fantasy 2</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/Final fantasy 3.jpg" alt="Final Fantasy" />
+                <p>Final Fantasy 3</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/Final fantasy 4.jpg" alt="Final Fantasy" />
+                <p>Final Fantasy 4</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/Final fantasy 5.jpg" alt="Final Fantasy" />
+                <p>Final Fantasy 5</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/Final fantasy 6.jpeg" alt="Final Fantasy" />
+                <p>Final Fantasy 6</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/Final-Fantasy-7-Concept-Art-Cover.jpg" alt="Final Fantasy" />
+                <p>Final Fantasy 7</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/gilang-febrian-oppasa-beach.jpg" alt="Chrono Cross" />
+                <p>Chrono Cross</p>
+              </div>
+              <div className='media-element'>
+                <img src="/images/wallpaperflare.com_wallpaper.jpg" alt="Metroid" />
+                <p>Metroid</p>
+              </div>
+            </div>
+          </div>
+    
+        </div>
+      )
+}
